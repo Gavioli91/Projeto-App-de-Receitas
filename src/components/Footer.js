b2ef703e2adf775d008d59863fcd5c8ff97a1cc1
Ricardo_Souza <rl03souza@gmail.com>
2022-09-26 16:25:27 -0300

    adding requirements 16, 17 and 18

---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import DrinkIcon from '../images/drinkIcon.svg';
+import MealIcon from '../images/mealIcon.svg';
+import '../App.css';
+
+function Footer() {
+  const history = useHistory();
+
+  const redirectTo = (pathName) => {
+    history.push(pathName);
+  };
+
+  return (
+    <footer className="footer-container" data-testid="footer">
+      <input
+        type="image"
+        onClick={ () => redirectTo('/drinks') }
+        src={ DrinkIcon }
+        alt="drinks screen"
+        data-testid="drinks-bottom-btn"
+      />
+      <input
+        type="image"
+        onClick={ () => redirectTo('/meals') }
+        src={ MealIcon }
+        alt="foods screen"
+        data-testid="meals-bottom-btn"
+      />
+    </footer>
+  );
+}
+
+export default Footer;
