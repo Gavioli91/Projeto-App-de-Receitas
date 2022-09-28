b035d358fa2d1b5e6ee0a5bea0a8dce4afe2b042
Fillipe Oliveira <fillipe.trybe@gmail.com>
2022-09-28 18:39:29 -0300

    remove console.log

--- src/utils/fetchRecipesDetails.js
+++ src/utils/fetchRecipesDetails.js
@@ -2,14 +2,12 @@ const BASE_URL_MEALS = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
 const BASE_URL_DRINKS = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
 export const getMealsDetails = async (id) => {
-  console.log(id);
   const response = await fetch(`${BASE_URL_MEALS}${id}`);
   const data = await response.json();
   return data;
 };
 
 export const getDrinksDetails = async (id) => {
-  console.log(id);
   const response = await fetch(`${BASE_URL_DRINKS}${id}`);
   const data = await response.json();
   return data;
