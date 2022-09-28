a4f4e30510022f90c5d9725fb91be55ba429f106
Fillipe Oliveira <fillipe.trybe@gmail.com>
2022-09-28 15:21:45 -0300

    pull

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import SearchBarProvider from './context/SearchBarProvider';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <SearchBarProvider>
+      <App />
+    </SearchBarProvider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
